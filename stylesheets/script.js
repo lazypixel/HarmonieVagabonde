0ddacc2fde733c967deda88cfe130479751b59d8
Chris <133039386+lazypixel@users.noreply.github.com>
2024-08-07 18:57:45 +0200

    Add files via upload

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -22,27 +22,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
-
-// Touch event variables
-    let touchStartX = 0;
-    let touchEndX = 0;
-
-    // Detect touch start
-    sliderContainer.addEventListener('touchstart', (e) => {
-        touchStartX = e.changedTouches[0].screenX;
-    });
-
-    // Detect touch end
-    sliderContainer.addEventListener('touchend', (e) => {
-        touchEndX = e.changedTouches[0].screenX;
-        handleGesture();
-    });
-
-    function handleGesture() {
-        if (touchEndX < touchStartX) {
-            nextSlide();
-        }
-        if (touchEndX > touchStartX) {
-            prevSlide();
-        }
-    }
